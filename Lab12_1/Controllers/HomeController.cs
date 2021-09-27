18663bf02ed428acc24af937e1173622fb739422
Tristin Bruyere <TristinBruyere@quickenloans.com>
2021-09-27 17:13:35 -0400

    First commit of assessment 12_1

---
+++ Lab12_1/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Lab12_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Lab12_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ShuffleDeck()
+        {
+            string domain = "https://deckofcardsapi.com";
+            string path = "/api/deck/new/shuffle/?deck_count=1";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path);
+            Deck result = await connection.Content.ReadAsAsync<Deck>();
+            return View(result);
+        }
+
+        public async Task<IActionResult> DrawFive(string id)
+        {
+            string domain = "https://deckofcardsapi.com";
+            string path = $"/api/deck/{id}/draw/?count=5";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path);
+            Hand result = await connection.Content.ReadAsAsync<Hand>();
+            return View(result);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab12_1/Models/DeckOfCards.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12_1.Models
+{
+    public class Deck
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public int remaining { get; set; }
+        public bool shuffled { get; set; }
+    }
+    public class Hand
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public List<Cards> cards { get; set; }
+        public int remaining { get; set; }
+    }
+    public class Cards
+    {
+        public string code { get; set; }
+        public string image { get; set; }
+        //public List<Images> images { get; set; }
+        public string value { get; set; }
+        public string suit { get; set; }
+
+    }
+    public class Images
+    {
+        public string svg { get; set; }
+        public string png { get; set; }
+    }
+}
